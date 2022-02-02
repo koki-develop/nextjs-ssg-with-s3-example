c95bd43f9f7422415be0078d7c51cd2944a07871
koki-develop <kou.pg.0131@gmail.com>
2022-02-03 08:22:58 +0900

    静的ファイルを配信する CloudFront を作成

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,6 +3,23 @@ resource "aws_s3_bucket" "frontend" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = data.aws_iam_policy_document.frontend.json
+}
+
+data "aws_iam_policy_document" "frontend" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.frontend.iam_arn]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.frontend.arn}/*"]
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket                  = aws_s3_bucket.frontend.id
   block_public_acls       = true
